c25f3f3ee0a9cbb1bb30da9c5ad7e79cbc7e56be
s-yenugan <syenugan@gmail.com>
2025-04-04 21:10:26 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+#Task 1: Perform Basic Mathematical Operations
+a = input ("Enter the first number: ")
+b = input("Enter the second number: ")
+a = int(a)
+b = int(b)
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print ('Addition: ', c, '\nSubtraction: ', d, '\nMultiplication: ', e , '\nDivision: ', f)
+
+
+#Task 2: Create a Personalized Greeting
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+print('Hello,',a,b,'!', 'Welcome to Python Programming')
